ce8d1dc1c9337cb9fa08b8dcfee5cd3f0a65a0f0
demur <demur2000@mail.ru>
2020-05-03 00:19:41 -0400

    fix: Replace deprecated method AndroidHttp.newCompatibleTransport()

--- app/src/main/java/com/udacity/demur/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/demur/builditbigger/EndpointsAsyncTask.java
@@ -2,10 +2,10 @@ package com.udacity.demur.builditbigger;
 
 import android.os.AsyncTask;
 
-import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.udacity.demur.builditbigger.backend.myApi.MyApi;
 
 import java.io.IOException;
@@ -18,7 +18,7 @@ public class EndpointsAsyncTask extends AsyncTask<EndpointsAsyncTask.JokeCallbac
     @Override
     protected String doInBackground(JokeCallback... params) {
         if (null == myApiService) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+            MyApi.Builder builder = new MyApi.Builder(new NetHttpTransport(),
                     new AndroidJsonFactory(), null)
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
